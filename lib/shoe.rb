dcd8124f89e4bbfbc1781042e4e2b22482faebc1
Alexander Marusin <amarusin18@gmail.com>
2020-07-26 02:03:33 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,9 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-  BRANDS.uniq
+  
   BRANDS = []
-
+  BRANDS & BRANDS
 
   def initialize(brand)
     @brand = brand
